952d117039703ce04fa00965f4d57207c5c91f55
Elina Iskhakova <35336512+ElinaIskhakova@users.noreply.github.com>
2018-07-03 16:08:27 +0500

    Add files via upload

--- RaschetDlinElementov.py
+++ RaschetDlinElementov.py
@@ -1,15 +1,7 @@
 import sys
 from math import *
-def h2_1(Ln,h1,ht):
-    return round(ceil(Ln-h1-ht)/10)*10
 
-def h2_2(Lk,h1,ht):
-    return round(ceil(Lk-h1-ht)/10)*10
+def e2_1(Ln,h1,ht,l1):
+    return (round(ceil(Ln-h1-ht)/10)*10-l1)
 
-def e2_1(h2,l1):
-    return (h2-l1)
-
-def e2_2(h2,l1):
-    return (h2-l1)
-
-print(h2_2(4300,75,29.950))
+print(e2_1(3800,75,24.950,1600))
